3e4ff2d25a2f31e4a4461b502327131b5d2e8c05
Elena Damdinova <elenadamdinova@gmail.com>
2021-01-18 20:57:20 +0800

    Заменила дубликаты

--- src/main/java/ru/netology/stats/StatsService.java
+++ src/main/java/ru/netology/stats/StatsService.java
@@ -1,74 +1,62 @@
 package ru.netology.stats;
 
 public class StatsService {
-    public long calculateSum(long[] values) {
+    public long calculateSum(long[] purchases) {
         long sum = 0;
-        for (long value: values) {
-            sum = sum + value;
+        for (long purchase : purchases) {
+            sum = sum + purchase;
         }
         return sum;
     }
 
-    public long calculateAverageValue(long[] values) {
-        long sum = 0;
-        for (long value: values) {
-            sum = sum + value;
-        }
-        return sum / 12;
+    public long calculateAverage(long[] purchases) {
+        return calculateSum(purchases) / purchases.length;
     }
 
-    public int findMonthNumberWithMaxPurchases(long[] values) {
+    public int findMonthNumberWithMaxPurchases(long[] purchases) {
         int month = 0;
         int number = 1;
-        long maxValue = values[0];
-        for (long value: values) {
+        long maxValue = purchases[0];
+        for (long purchase : purchases) {
             month = month + 1;
-            if (value >= maxValue) {
-                maxValue = value;
+            if (purchase >= maxValue) {
+                maxValue = purchase;
                 number = month;
             }
         }
         return number;
     }
 
-    public int findMonthNumberWithMinPurchases(long[] values) {
+    public int findMonthNumberWithMinPurchases(long[] purchases) {
         int month = 0;
         int number = 1;
-        long minValue = values[0];
-        for (long value : values) {
+        long minValue = purchases[0];
+        for (long purchase : purchases) {
             month = month + 1;
-            if (value <= minValue) {
-                minValue = value;
+            if (purchase <= minValue) {
+                minValue = purchase;
                 number = month;
             }
         }
         return number;
     }
 
-    public int calculateAmountMonthLessAverage(long[] values) {
-        long sum = 0;
-        for (long value : values) {
-            sum = sum + value;
-        }
-        long average = sum / 12;
+    public int calculateAmountMonthLessAverage(long[] purchases) {
+        long average = calculateAverage(purchases);
         int amount = 0;
-        for (long value : values) {
-            if (value < average) {
+        for (long purchase : purchases) {
+            if (purchase < average) {
                 amount = amount + 1;
             }
         }
         return amount;
     }
 
-    public int calculateAmountMonthOverAverage(long[] values) {
-        long sum = 0;
-        for (long value : values) {
-            sum = sum + value;
-        }
-        long average = sum / 12;
+    public int calculateAmountMonthOverAverage(long[] purchases) {
+        long average = calculateAverage(purchases);
         int amount = 0;
-        for (long value : values) {
-            if (value > average) {
+        for (long purchase : purchases) {
+            if (purchase > average) {
                 amount = amount + 1;
             }
         }
--- src/test/java/StatsServiceTest.java
+++ src/test/java/StatsServiceTest.java
@@ -10,54 +10,54 @@ public class StatsServiceTest {
     @Test
     void testCalculateSum() {
         StatsService service = new StatsService();
-        long[] values = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
+        long[] purchases = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
         long expected = 180;
-        long actual = service.calculateSum(values);
+        long actual = service.calculateSum(purchases);
         assertEquals(expected, actual);
     }
 
     @Test
     void testCalculateAverageValue() {
         StatsService service = new StatsService();
-        long[] values = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
+        long[] purchases = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
         long expected = 15;
-        long actual = service.calculateAverageValue(values);
+        long actual = service.calculateAverage(purchases);
         assertEquals(expected, actual);
     }
 
     @Test
     void testFindMonthNumberWithMaxPurchases() {
         StatsService service = new StatsService();
-        long[] values = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
+        long[] purchases = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
         long expected = 8;
-        long actual = service.findMonthNumberWithMaxPurchases(values);
+        long actual = service.findMonthNumberWithMaxPurchases(purchases);
         assertEquals(expected, actual);
     }
 
     @Test
     void testFindMonthNumberWithMinPurchases() {
         StatsService service = new StatsService();
-        long[] values = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
+        long[] purchases = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
         long expected = 9;
-        long actual = service.findMonthNumberWithMinPurchases(values);
+        long actual = service.findMonthNumberWithMinPurchases(purchases);
         assertEquals(expected, actual);
     }
 
     @Test
     void testCalculateAmountMonthLessAverage() {
         StatsService service = new StatsService();
-        long[] values = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
+        long[] purchases = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
         long expected = 5;
-        long actual = service.calculateAmountMonthLessAverage(values);
+        long actual = service.calculateAmountMonthLessAverage(purchases);
         assertEquals(expected, actual);
     }
 
     @Test
     void testCalculateAmountMonthOverAverage() {
         StatsService service = new StatsService();
-        long[] values = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
+        long[] purchases = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
         long expected = 5;
-        long actual = service.calculateAmountMonthOverAverage(values);
+        long actual = service.calculateAmountMonthOverAverage(purchases);
         assertEquals(expected, actual);
     }
 }
